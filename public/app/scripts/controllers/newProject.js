511578b69babc9681d97fd0aefecd9b5c4027e54
Eric Fischer <ericf712@gmail.com>
2015-10-08 14:34:59 -0700

    Formatted table in index.html and details.html

--- public/app/scripts/controllers/newProject.js
+++ public/app/scripts/controllers/newProject.js
@@ -1,8 +1,15 @@
 app.controller('NewProject', function($scope, $location, Project) {
   $scope.project = {};
+
   $scope.save = function() {
     Project.save($scope.project, function(){
       $location.path('/');
     });
   }
+  
+  $scope.destroy = function() {
+    self.original.destroy(function() {
+      $location.path('/');
+    });
+  };
 });
