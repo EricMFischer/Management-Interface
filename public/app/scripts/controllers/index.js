7679cddb2099d41c775e8f6391f2a4cb37361142
Eric Fischer <ericf712@gmail.com>
2015-10-11 14:52:40 -0700

    Edited index.html to bind with new project props

--- public/app/scripts/controllers/index.js
+++ public/app/scripts/controllers/index.js
@@ -1,7 +1,7 @@
 app.controller('Index', function($scope, $rootScope, Project) {
 
-  // $scope.projects = Project.query(); // an array
-  $scope.projects = Project.prototype.projects;
-  console.log('$scope.projects: ', $scope.projects);
+  $scope.projects = Project.query(); // an array
+  // $scope.projects = Project.prototype.projects;
+  // console.log('$scope.projects: ', $scope.projects);
 
 });
